1873fbb9d0e7f4ba0e3e4174380eb6c2f5dc7f11
Bishop-dev <alex.bishop@yandex.ru>
2016-09-01 22:46:56 +0300

    Fixed dates in records list

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module("app", ["ngRoute", "ngResource", "app.spendings"])
+        .module("app", ["ngRoute", "ngResource", "app.pagination", "app.spendings"])
         .controller("AppCtrl", [AppCtrl]);
 
     function AppCtrl() {
--- src/main/resources/static/js/app/util/pagination-directive.js
+++ src/main/resources/static/js/app/util/pagination-directive.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module("app")
+        .module("app.pagination")
         .directive("pagination", [pagination]);
 
     function pagination(){
